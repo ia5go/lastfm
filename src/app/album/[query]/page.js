2c8ad3f9dc0bde87099b46fe1d40ee4a385874c5
ia5go <benvindoiago@outlook.com>
2024-07-05 18:04:01 -0300

    Home e Album
    Criação de interação de querys e links na home e criação de roteamento p/ pg de album;
    
    Co-Authored-By: DekiWill <89227114+DekiWill@users.noreply.github.com>

---
+++ src/app/album/[query]/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { Form } from "@/components/Form";
+
+export default function Album({ params }) {
+  console.log(params);
+
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,35 @@
-import styles from "./Form.module.scss";
-import Button from "./button";
+"use client";
 
-export function Form(){
+import styles from "./Form.module.scss";
+import Button from "./Button";
+import { useState } from "react";
+
+export function Form() {
+  const key = "23328c9ce3416834fbc11fe8e263a75c";
+  const [search, setSearch] = useState("");
 
   return (
     <div className={styles.form}>
-      <Button title='Album' link="#"/>
+      <Button
+        title="Album"
+        // link={`http://ws.audioscrobbler.com/2.0/?method=album.search&$album=${search}&limit=5&api_key=${key}&format=json`}
+        link={`/album/${search}`}
+      />
 
-      <input type="text" className={styles.form__input} placeholder="Search your singer or an album" />
-      
-      <Button title='Artist' link="#"/>
+      <input
+        value={search}
+        type="text"
+        className={styles.form__input}
+        placeholder="Search your singer or an album"
+        onChange={(ev) => setSearch(ev.target.value.trim())}
+      />
+
+      <Button
+        title="Artist"
+        link={`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${search}&limit=5&api_key=${key}&format=json`}
+      />
     </div>
-  )
-};
-
+  );
+}
+// use state / use effect para capturar o value do input
+// use effect vai fazer o link e o fetch vai ser realizado na página interna
